73ce4c3a3db33272636f9ce59180928960348c35
Saima Rahman <rahman.saima92@gmail.com>
2019-10-28 18:17:27 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,69 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  newArray = []
+  i = 0
+  while i < src.length do
+    subArr = src[i]
+    j = 0
+
+  while j < subArr.length-1 do
+    ele = subArr[j]
+    nextele = subArr[j + 1]
+    newArray.push("I love #{ele} and #{nextele} on my pizza")
+    j+= 1
+  end
+  i+= 1
+end
+return newArray
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+   new_array = []
+  i = 0
+
+  while i < src.length do
+    subArr = src[i]
+    j = 0
+
+  while j < subArr.length-1 do
+     num = subArr[j]
+     nextNum= subArr[j + 1]
+
+    if num > nextNum
+      new_array.push(num)
+
+    elsif nextNum > num
+      new_array.push(nextNum)
+    
+    elsif num == nextNum
+      new_array.push(num)
+    end
+  end
+    j+= 1
+  end
+  i+=1
+  end
+  return new_array
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total = 0
+i = 0
+  while i < src.length do
+    subArr = src[i]
+    j= 0
+
+  while j < subArr.length do
+    num = subArr[j]
+
+    if num.even?
+
+      total += num
+
+    end
+    j+=1
+  end
+  i+=1
+  end
+  return total
+
 end

4d4ef5e1eb5395c3b5612a5843cf67c4009c3784
Saima Rahman <rahman.saima92@gmail.com>
2019-10-28 18:29:15 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,19 +45,21 @@ def find_greater_pair(src)
   return new_array
 end
 
+
 def total_even_pairs(src)
-  total = 0
+total = 0
 i = 0
   while i < src.length do
     subArr = src[i]
     j= 0
 
-  while j < subArr.length do
+  while j < subArr.length-1 do
     num = subArr[j]
+    nextNum = subArr[j + 1]
 
-    if num.even?
+    if num % 2 == 0 && nextNum % 2 == 0
 
-      total += num
+      total += num + nextNum
 
     end
     j+=1
@@ -65,5 +67,7 @@ i = 0
   i+=1
   end
   return total
+end
+
 
 end
